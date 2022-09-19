b1ef4378dca4f9d702873cc2293112e0a21779ca
Wilber Millan <yilbernet@gmail.com>
2022-09-18 21:02:37 -0500

    Primer Entregable

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import 'boxicons'
 
 const QuoteBox = ({randomQuote, randomColor, getRandomAll}) => {
 
@@ -9,11 +10,20 @@ const QuoteBox = ({randomQuote, randomColor, getRandomAll}) => {
    const backgroundObj = {
       backgroundColor: randomColor
    };
-
+   
   return (
     <article style={colorObj} className='card'>
-      <p className="card__quote">{randomQuote.quote}</p>
+      <p className="card__quote">{'" '+randomQuote.quote+' "'}</p>
       <h1 className="card__author">{randomQuote.author}</h1>
+      <box-icon
+         style={backgroundObj}
+         className='card__icon'
+         type='solid'
+         name='florist'
+         flip='horizontal'
+         animation='burst'
+      >
+      </box-icon>
       <button
          style={backgroundObj}
          className='card__btn'
